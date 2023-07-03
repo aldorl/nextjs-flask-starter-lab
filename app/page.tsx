f526426bd51f56e3c3e090687ff24f8c600234a6
Aldo Ruiz Luna <aldo.ruiz.luna@gmail.com>
2023-07-03 14:11:32 -0600

    Optimizes page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 'use client'
 
 import Button from './components/Button'
+import Message from './components/Message'
 import useDownloadVideo from './hooks/useDownloadVideo';
 
 export default function Home() {
-  const { downloading, downloaded, downloadVideo, setDownloaded } = useDownloadVideo();
+  const { downloading, downloaded, downloadVideo } = useDownloadVideo();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {downloaded && <Message variant="success">Download successful!</Message>}
       <Button
         disabled={downloading}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" 
