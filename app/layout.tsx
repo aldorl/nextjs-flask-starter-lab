72a888cf42e8566c301565aff39af88603d6c78d
Aldo Ruiz Luna <aldo.ruiz.luna@gmail.com>
2023-09-06 22:26:17 -0600

    Introduces some codebase changes n test render api

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,26 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+// Import the necessary modules and styles
+import './globals.css'  // Global styles
+import { Inter } from 'next/font/google'  // Google font
 
+// Define the Inter font with the 'latin' subset
 const inter = Inter({ subsets: ['latin'] })
 
+// Define the metadata for the application
 export const metadata = {
-  title: 'Next.JS & Flask TweetTok App',
-  description: 'Next.JS & Flask TweetTok App by Auroraa Innovation Labs & Aldo Ruiz Luna',
+  title: 'Next.JS & Flask TweetTok App',  // Title of the application
+  description: 'Next.JS & Flask TweetTok App by Auroraa Innovation Labs & Aldo Ruiz Luna',  // Description of the application
 }
 
+// Define the RootLayout component
 export default function RootLayout({
-  children,
+  children,  // Children to be rendered inside the layout
 }: {
-  children: React.ReactNode
+  children: React.ReactNode  // Type definition for the children prop
 }) {
+  // Return the JSX to be rendered
   return (
     <html lang="en">
+      {/* Use the Inter font for the body */}
       <body className={inter.className}>{children}</body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,41 @@
-'use client'
+'use client' // is a directive that can be used...
 
-import { useEffect } from 'react'
-import Alert from './components/Alert'
-import Button from './components/Button'
-import useDownloadVideo from './hooks/useDownloadVideo';
+// Import the necessary modules and components
+import { useEffect } from 'react'  // Hook for handling side effects
+import Alert from './components/Alert'  // Alert component for displaying messages
+import Button from './components/Button'  // Button component
+import useDownloadVideo from './hooks/useDownloadVideo';  // Custom hook for handling video download
 
+// Define the Home component
 export default function Home() {
+  // Destructure the variables and functions returned by the useDownloadVideo hook
   const { downloading, downloaded, downloadVideo, setDownloaded } = useDownloadVideo();
 
+  // useEffect hook to handle side effects
   useEffect(() => {
+    // If the video has been downloaded
     if (downloaded) {
+      // Set a timer to reset the downloaded state after 5 seconds
       const timer = setTimeout(() => setDownloaded(false), 5000);
+      
+      // Cleanup function to clear the timer
       return () => clearTimeout(timer);
     }
-  }, [downloaded]);
+  }, [downloaded]);  // Dependency array for the useEffect hook. The hook will run again if the 'downloaded' state changes.
 
+  // Return the JSX to be rendered
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {/* <Alert variant="info">This is relevant information</Alert> */}
+      {/* Alert component to display a success message when a download is successful */}
       {downloaded && <Alert variant="success">Download successful!</Alert>}
       {/* <Alert variant="error">Download unsuccessful...</Alert> */}
-      
+
+      {/* Button component to start the video download */}
       <Button
-        disabled={downloading}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" 
-        onClick={downloadVideo}
+        disabled={downloading}  // Disable the button while a download is in progress
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={downloadVideo}  // Set the onClick handler to the downloadVideo function
       >
         Download Now ⬇️
       </Button>
